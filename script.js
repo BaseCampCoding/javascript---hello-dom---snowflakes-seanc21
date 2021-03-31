4e259718da2ef4b17eadd48a217f1e2d87728116
Sean Coh <scoh21@basecampcodingacademy.org>
2021-03-31 15:32:06 -0500

    working on quantity slider

--- script.js
+++ script.js
@@ -4,15 +4,39 @@ const MIN_DURATION = 2000;
 const MAX_DURATION = 5000;
 
 const snowflakesContainer = document.getElementById("snowflakes-container");
+const snowVsRain = document.querySelector("#snow-vs-rain");
 
-setInterval(() => createSnowflake(), 50);
+const clickColor = document.querySelector("#click-color");
+
+const wind = document.querySelector("#wind");
+
+// quantity
+let quantityAmount = 250;
+let quantity = document.getElementById("quantity");
+let time = setInterval(() => createSnowflake(), quantityAmount);
+quantity.addEventListener("change", () => {
+  switch (quantity.value) {
+    case "0":
+      quantityAmount = 500;
+    case "1":
+      quantityAmount = 250;
+    case "2":
+      quantityAmount = 100;
+    case "3":
+      quantityAmount = 50;
+    case "4":
+      quantityAmount = 0;
+  }
+  clearInterval(time)
+  time = setInterval(() => createSnowflake(), quantityAmount);
+});
 
 function randint(lo, hi) {
   return Math.random() * (hi - lo) + lo;
 }
 
 function randomIcon() {
-  if (Math.random() < 0.5) {
+  if (Math.random() > Number(snowVsRain.value)) {
     return "fa-snowflake";
   } else {
     return "fa-tint";
